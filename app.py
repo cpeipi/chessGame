73f5f0b3051613f3c5035465e3d5a19555d457eb
Cody <codypeipi@gmail.com>
2025-08-05 15:20:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    serve(app, host="0.0.0.0", port=8000)
