6a6bca4ed5545ced97a4a149f7af454c440a6087
Cody <codypeipi@gmail.com>
2025-08-05 14:40:39 +0800

    hosting server script

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from flask  import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+
+import pygame 
